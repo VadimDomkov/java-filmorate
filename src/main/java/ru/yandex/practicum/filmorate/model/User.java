d0ed2565a1e60d21c92f9f91c42a9f94f411801d
VadimDomkov <vdomkov@yandex.ru>
2023-08-11 17:40:12 +0300

    fix : birthday

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -6,7 +6,7 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Pattern;
-import java.util.Date;
+import java.time.LocalDate;
 
 @Data
 public class User {
@@ -19,7 +19,7 @@ public class User {
     private String login;
     private String name;
     @Past
-    private Date birthday;
+    private LocalDate birthday;
 
     public boolean isNameNull() {
         return name == null;
