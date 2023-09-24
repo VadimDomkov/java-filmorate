91a5ce2645a2c8aeffdf35d2c4606aa0bd44c197
VadimDomkov <vdomkov@yandex.ru>
2023-09-24 19:24:35 +0300

    feat : add database

--- src/test/java/ru/yandex/practicum/filmorate/storage/FilmsDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/FilmsDbStorageTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import ru.yandex.practicum.filmorate.exceptions.ElementNotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.MPA;
@@ -16,6 +17,7 @@ import java.time.LocalDate;
 @SpringBootTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class FilmsDbStorageTest {
     private final FilmDbStorage filmDbStorage;
 
